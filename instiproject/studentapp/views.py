357898a3b1327cdc0663132710ba06c95ad26945
maria <maria.mariageorge123@gmail.com>
2019-12-03 20:55:45 +0530

    this is my first command

---
+++ instiproject/studentapp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.shortcuts import render
+from django.shortcuts import render
+from django.urls import reverse_lazy
+
+from adminapp.models import User,Course,Batch,Role,TimeTable,Placement,ClassRoom,Hr,Trainer,Feedback
+from django.views.generic import TemplateView, ListView, CreateView
+from django.views import generic
+from adminapp.models import Course
+# Create your views here.
+
+class studentget_home(TemplateView):
+
+    template_name = 'adminapp/studentbase.html'
+
+class StudentFeedback(CreateView):
+    model=Feedback
+
+    fields=['user_name','batch_name','feedback_data','trainer_name','feedback_date']
+    template_name = 'adminapp/studentfeedback.html'
+    success_url=reverse_lazy('studentfeedback')
+
+
+
+
+
+
+
+# Create your views here.
+
+# Create your views here.
